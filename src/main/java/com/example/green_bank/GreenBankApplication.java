05c89c0dd7f86da4d75214d9ecaa3254dce99c6e
Dayeong-dev <ydiosa98@gmail.com>
2025-06-20 11:30:10 +0900

    first commit

---
+++ src/main/java/com/example/green_bank/GreenBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.green_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBankApplication.class, args);
+	}
+
+}
