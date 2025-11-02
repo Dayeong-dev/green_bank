ff79b9c4ef86573a549cd9246969b1eec1c5e704
Yun Dayeong <ydiosa98@gmail.com>
2025-11-02 18:06:48 +0900

    fix: NotificationDTO 내 엔티티 필드 > DTO로 변경

--- src/main/java/com/example/green_bank/questions/dto/NotificationDTO.java
+++ src/main/java/com/example/green_bank/questions/dto/NotificationDTO.java
@@ -1,5 +1,6 @@
 package com.example.green_bank.questions.dto;
 
+import com.example.green_bank.user.dto.UserDTO;
 import com.example.green_bank.user.entity.User;
 import lombok.Data;
 
@@ -9,5 +10,5 @@ public class NotificationDTO {
     private String message;
     private String targetUrl;
     private Integer isRead;
-    private User user;
+    private UserDTO userDTO;
 }
--- src/main/java/com/example/green_bank/questions/util/NotificationMapper.java
+++ src/main/java/com/example/green_bank/questions/util/NotificationMapper.java
@@ -2,28 +2,38 @@ package com.example.green_bank.questions.util;
 
 import com.example.green_bank.questions.dto.NotificationDTO;
 import com.example.green_bank.questions.entity.Notification;
+import com.example.green_bank.user.util.EntityDtoMapper;
 
 public class NotificationMapper {
+	
+	private EntityDtoMapper userMapper = new EntityDtoMapper();
+	
     public Notification toEntity(NotificationDTO notificationDTO) {
+    	
+    	if(notificationDTO == null) return null;
+    	
         Notification notification = new Notification();
 
         notification.setNno(notificationDTO.getNno());
         notification.setMessage(notificationDTO.getMessage());
         notification.setTargetUrl(notificationDTO.getTargetUrl());
         notification.setIsRead(notificationDTO.getIsRead());
-        notification.setUser(notificationDTO.getUser());
+        notification.setUser(userMapper.toEntity(notificationDTO.getUserDTO()));
 
         return notification;
     }
 
     public NotificationDTO toDTO(Notification notification) {
+    	
+    	if(notification == null) return null;
+    	
         NotificationDTO notificationDTO = new NotificationDTO();
 
         notificationDTO.setNno(notification.getNno());
         notificationDTO.setMessage(notification.getMessage());
         notificationDTO.setTargetUrl(notification.getTargetUrl());
         notificationDTO.setIsRead(notification.getIsRead());
-        notificationDTO.setUser(notification.getUser());
+        notificationDTO.setUserDTO(userMapper.toDTO(notification.getUser()));
 
         return notificationDTO;
     }
